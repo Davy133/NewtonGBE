0794345670293bb920b0a02eb871754ff3a928ee
Davy133 <davy.coelho@raio.tech>
2023-10-22 23:11:25 -0300

    add common

---
+++ include/common.h
@@ -0,0 +1,13 @@
+//Loads basic libraries and defines basic types.
+
+#pragma once
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdint.h>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
